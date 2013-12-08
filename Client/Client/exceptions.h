db5e8c03af91ec7343e616b4e2a566ff5128349c
alekseyl1992 <alekseyl@list.ru>
2013-12-08 20:35:34 +0400

    exceptions.h added

---
+++ Client/Client/exceptions.h
@@ -0,0 +1,23 @@
+#ifndef EXCEPTIONS_H
+#define EXCEPTIONS_H
+#include <exception>
+#include <string>
+
+class ResourceException : std::exception
+{
+public:
+
+    ResourceException(const char *str)
+        : _what(str)
+    {}
+
+    const char *what() const throw()
+    {
+        return _what.c_str();
+    }
+
+private:
+    std::string _what;
+};
+
+#endif // EXCEPTIONS_H
