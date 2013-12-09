1deeb962d863bccf5139a8d85e7f85ca51827529
alekseyl1992 <alekseyl@list.ru>
2013-12-09 19:08:11 +0400

    Required libs added to /bin

---
+++ start.bat
@@ -0,0 +1,3 @@
+start /D "C:\Projects\Qt\NeonHockey\Server\build-Server-Desktop_Qt_5_1_1_MinGW_32bit-Debug\debug\" Server.exe
+start /D "C:\Projects\Qt\NeonHockey\Client\build-Client-Desktop_Qt_5_1_1_MinGW_32bit-Debug\debug\" Client.exe
+start /D "C:\Projects\Qt\NeonHockey\Client\build-Client-Desktop_Qt_5_1_1_MinGW_32bit-Debug\debug\" Client.exe
